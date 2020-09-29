21d52c2565228fe0f37479d1e6aa39ca0f04e587
Patryk Szwed <patryk.szwed@outlook.com>
2020-09-29 21:08:36 +0200

    Fixed default remaining tries constant

--- src/components/dice-roll/constants/constants.js
+++ src/components/dice-roll/constants/constants.js
@@ -1,7 +1,7 @@
 export const DICE_IMAGE_WIDTH = 100;
 export const DICE_IMAGE_HEIGHT = 100;
 
-export const REMAINING_TRIES_ON_START = 5;
+export const REMAINING_TRIES_ON_START = 30;
 export const POINTS_PER_CORRECT_PREDICTION = 0.1;
 
 export const PREDICTION = {
